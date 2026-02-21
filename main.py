7bd49a3ccffa90a932e1cddbd3337a0472847fff
Brian Dorantes <briandorantes@hotmail.com>
2026-02-21 11:25:34 -0600

    correcion fechas y agregacion hcapa digital

--- main.py
+++ main.py
@@ -212,6 +212,12 @@ def procesar_ventas(spreadsheet_id, sheet_name, fecha_ini=None, fecha_fin=None):
     df.loc[mask, "Fecha de captura"] = fecha_mayor
     df.loc[mask, "Fecha de venta"] = fecha_mayor
     df.drop(columns=['Saldo Restante','Fecha de captura','Importe Total'], inplace=True)
+    df = df[~(
+    ((df["Tipo de Pago"] == "Puerta pagada (anticipo)") &
+     (df["Articulo"] == "Instalacion")) |
+
+    ((df["Tipo de Pago"] == "Instalación") &
+     (df["Articulo"] != "Instalacion")))]
 
     
 
